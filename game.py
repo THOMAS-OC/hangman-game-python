9dca4d83cd463b9a57dc1ed47eb902c08f751091
ESTIVAL Thomas <thomasestivaloc@gmail.com>
2022-10-15 16:11:08 +0200

    Lecture d'un dico de mot et extraction d'un mot aléatoire

--- game.py
+++ game.py
@@ -1,1 +1,11 @@
+"""Le but du jeu est simple : deviner toute les lettres qui doivent composer un mot, 
+éventuellement avec un nombre limité de tentatives et des thèmes fixés à l’avance. 
+A chaque fois que le joueur devine une lettre, celle-ci est affichée. 
+Dans le cas contraire, le dessin d’un pendu se met à apparaître…"""
+import random as r
 
+file = open("mots.txt", "r")
+
+mots = file.readlines()
+
+print(r.choice(mots))
